c52a806a98ed4cc8648f296bca08730ff7e9c3c6
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-08-21 16:41:05 -0500

    refactor main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx"
+import Error from "./pages/Error.jsx"
+
+const router = createBrowserRouter ([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: "/About",
+        element: <About />
+      }
+    ]
+  }
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
