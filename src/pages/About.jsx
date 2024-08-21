fc0127f22911f57353d8ea5dbc501f38dfb189ed
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-08-21 16:41:25 -0500

    build About page

---
+++ src/pages/About.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/no-unescaped-entities */
+function About () {
+    return (
+        <section className="about-me section">
+            <div>
+                <h3>About Me</h3>
+            </div>
+            <p>Hi I'm Charles Nguyen, a Full-Stack Developer. I graduated from San Jose State University in 2022 with a
+                degree
+                in Applied Computational Mathematics with an emphasis in Software Programming. I am currently enhancing
+                my
+                skills by taking Full-Stack Development bootcamp at the University of California, Berkeley. <br /><br />
+                I enjoy building dynamic creative products from beginning to end. My focus is on developing intuitive
+                experiences that consistently grow and improve based on user metrics.Creating a user friendly app like
+                the Note
+                Taker; this application allows users to take, save, and trash the notes by utilizing express.js and
+                node.js.
+            </p>
+        </section>
+    );
+}
+
+export default About;
