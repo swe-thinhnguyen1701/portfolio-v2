d39ef166c087993dacac9894976028d71cad81c1
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-08-21 19:15:11 -0500

    update About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,20 +1,15 @@
 /* eslint-disable react/no-unescaped-entities */
-function About () {
+function About() {
     return (
         <section className="about-me section">
             <div>
                 <h3>About Me</h3>
             </div>
-            <p>Hi I'm Charles Nguyen, a Full-Stack Developer. I graduated from San Jose State University in 2022 with a
-                degree
-                in Applied Computational Mathematics with an emphasis in Software Programming. I am currently enhancing
-                my
-                skills by taking Full-Stack Development bootcamp at the University of California, Berkeley. <br /><br />
-                I enjoy building dynamic creative products from beginning to end. My focus is on developing intuitive
-                experiences that consistently grow and improve based on user metrics.Creating a user friendly app like
-                the Note
-                Taker; this application allows users to take, save, and trash the notes by utilizing express.js and
-                node.js.
+            <p>Hi My name is Thinh Nguyen, and I also go by Charles. I am a passionate and dedicated full stack developer with a strong foundation in applied mathematics, having earned my bachelor's degree from San Jose State University. Currently, I am furthering my expertise by taking a full-stack developer bootcamp at UC Berkeley.<br />
+                I am proficient in a variety of programming languages including Java, JavaScript, Python, Lua, HTML, and CSS. In terms of databases, I have experience with PostgreSQL, MongoDB, and AWS DynamoDB. Additionally, I am well-versed in Node.js, Express, React, and several AWS technologies such as AWS Lambda and AWS Gateway. <br />
+                Although I am relatively new to the professional field, I have completed several projects during my time at the UC Berkeley bootcamp, as well as personal projects that have allowed me to hone my skills and develop a deeper understanding of full-stack development.<br />
+                Outside of my professional interests, I enjoy swimming and playing basketball. These activities help me maintain a balanced and healthy lifestyle, and they also provide me with opportunities to connect with others and have fun.<br />
+                I am enthusiastic about the future and look forward to contributing to innovative projects and working with talented teams to create impactful solutions.
             </p>
         </section>
     );
