3e377202311347a3627cd8d36bc6179a0d8b03e3
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-08-21 19:15:02 -0500

    update Error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,22 @@
-function Error () {
-    return <h2>Page not found</h2>
+import { useRouteError } from "react-router-dom";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+
+function Error() {
+    const error = useRouteError();
+    console.log(error);
+
+
+    return (
+        <>
+            <NavBar />
+            <section className="section">
+                <h2>ERROR</h2>
+                <p>{error.statusText || error.message}</p>
+            </section>
+            <Footer />
+        </>
+    )
 }
 
 export default Error;
